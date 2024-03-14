606f8e91da6bf3b7ba65cd94bdb3c43c96a0bc4c
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-03-14 10:24:18 +0900

    feat: 게시글 생성 api 추가 (#4)

---
+++ src/main/java/com/example/demo/src/board/model/PostBoardRes.java
@@ -0,0 +1,14 @@
+package com.example.demo.src.board.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostBoardRes {
+
+    private Long id;
+}

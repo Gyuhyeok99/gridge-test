4ff37316a37b3752a1ae0610eececb8a2c7c8fe5
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-03-19 08:41:51 +0900

    feat: 로컬, dev 분리 (#17)

--- src/main/java/com/example/demo/common/config/WebConfig.java
+++ src/main/java/com/example/demo/common/config/WebConfig.java
@@ -16,6 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                 // Header의 Origin에 들어있는 주소가 http://localhost:3000인 경우를 허용한다
                 .allowedOrigins("http://localhost:3000")
                 .allowedOrigins("http://localhost:63342")
+                .allowedOrigins("https://dev.gridgetest.shop")
                 // 모든 HTTP Method를 허용한다.
                 .allowedMethods("*")
                 // HTTP 요청의 Header에 어떤 값이든 들어갈 수 있도록 허용한다.

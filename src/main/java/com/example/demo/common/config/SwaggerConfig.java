3fcb9d66da8f7389db8b5baa9ff2607f403780d4
황규혁 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-03-19 08:42:42 +0900

    Ci/17 (#22)
    * feat: ci/cd 파이프라인 구축 (#17)
    
    * feat: ci/cd 파이프라인 구축 (#17)
    
    * feat: ci/cd 파이프라인 구축 (#17)
    
    * feat: 프로필 변경 (#17)
    
    * feat: rds 설정 추가 (#17)
    
    * feat: 로컬, dev 분리 (#17)

--- src/main/java/com/example/demo/common/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/common/config/SwaggerConfig.java
@@ -5,14 +5,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI(){
         return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .addServersItem(new Server().url(serverUrl))
                 .components(new Components().addSecuritySchemes("JWT", createAPIKeyScheme()))
                 .info(new Info().title("demo 서비스 API 명세서")
                         .description("스프링부트 demo 서비스 CRUD 실습 API 명세서")

8efeb4bfe8a9e0b5e41bbc1a70be4d5b714506dc
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-03-19 08:49:31 +0900

    refactor: 스웨거 설정 변경

--- src/main/java/com/example/demo/common/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/common/config/SwaggerConfig.java
@@ -6,29 +6,43 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig implements WebMvcConfigurer {
+    private static final String SECURITY_SCHEME_NAME = "authorization";	// 추가
 
-    @Value("${server.url}")
+    @Value("${swagger.server-url}")
     private String serverUrl;
 
     @Bean
-    public OpenAPI openAPI(){
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("JWT"))
-                .addServersItem(new Server().url(serverUrl))
-                .components(new Components().addSecuritySchemes("JWT", createAPIKeyScheme()))
-                .info(new Info().title("demo 서비스 API 명세서")
-                        .description("스프링부트 demo 서비스 CRUD 실습 API 명세서")
-                        .version("v1"));
+    public GroupedOpenApi jwtApi() {
+        return GroupedOpenApi.builder()
+                .group("jwt-api")
+                .pathsToMatch("/**")
+                .build();
     }
+    @Bean
+    public OpenAPI OpenApi() {
 
-    private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-                .bearerFormat("JWT")
-                .scheme("bearer");
+        return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl))
+                .components(new Components()
+                        // 여기부터 추가 부분
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                // 여기까지
+                .info(new Info()
+                        .title("demo 서비스 API 명세서")
+                        .description("스프링부트 demo 서비스 CRUD 실습 API 명세서")
+                        .version("V1"));
     }
 }
